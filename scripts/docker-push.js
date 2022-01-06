76d5e2e548ac026d0d1ae4ca74e69d6a34b88765
Monve <chenmenghui@lazbao.com>
2022-01-06 18:22:18 +0800

    fix: docker build --pull --rm -f "Dockerfile" -t monve/${name}:${version} "."

--- scripts/docker-push.js
+++ scripts/docker-push.js
@@ -2,7 +2,7 @@ const execSync = require('child_process').execSync
 
 const { version, name } = require('../package')
 
-let command = `docker tag monve/${name}:${version} monve/${name}:latest`
+let command = `docker build --pull --rm -f "Dockerfile" -t monve/${name}:${version} "."`
 console.log(command)
 execSync(command)
 
