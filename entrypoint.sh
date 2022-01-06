9ad979645971082406b68170dc091af421c19d50
Monve <chenmenghui@lazbao.com>
2022-01-06 17:30:02 +0800

    fix: entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+
+###
+ # @Author: Monve
+ # @Date: 2021-08-27 17:31:45
+ # @LastEditors: Monve
+ # @LastEditTime: 2021-08-30 15:03:03
+ # @FilePath: /lzb_lazada_crawler/entrypoint.sh
+### 
+
+#!/bin/bash
+
+npm run docker_start
