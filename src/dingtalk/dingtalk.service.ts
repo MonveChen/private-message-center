a3bb75c2e49c8ffb17aa18ec52823bd615a179ef
Monve <chenmenghui@lazbao.com>
2022-01-07 15:02:37 +0800

    fix: content decode to utf8

--- src/dingtalk/dingtalk.service.ts
+++ src/dingtalk/dingtalk.service.ts
@@ -2,7 +2,7 @@
  * @Author: Monve
  * @Date: 2022-01-06 11:29:50
  * @LastEditors: Monve
- * @LastEditTime: 2022-01-06 16:55:50
+ * @LastEditTime: 2022-01-07 15:00:36
  * @FilePath: /private-message-center/src/dingtalk/dingtalk.service.ts
  */
 import { Injectable } from '@nestjs/common';
@@ -10,6 +10,7 @@ import { DefaultResponse } from '../interface/response';
 import { ck } from '../utils/common';
 import { CODE, DING_CONFIG } from '../utils/const';
 import * as RobotDing from "@tnnevol/robot-ding";
+import { decode } from "utf8"
 
 const ding = process.env.WEBHOOK && process.env.SECRET ?
   new (RobotDing as any)(DING_CONFIG) : null
@@ -18,7 +19,7 @@ const ding = process.env.WEBHOOK && process.env.SECRET ?
 export class DingtalkService {
   async call({ content, webhook, secret, pv = null }): Promise<DefaultResponse> {
     const r = ck(content); if (r) { return r }
-
+    content = decode(content);
     const dingtalk = webhook && secret ?
       new (RobotDing as any)({ webhook, secret }) :
       !process.env.PV || process.env.PV === pv ? ding : null
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
  * @Author: Monve
  * @Date: 2022-01-06 11:10:17
  * @LastEditors: Monve
- * @LastEditTime: 2022-01-06 14:54:15
+ * @LastEditTime: 2022-01-07 14:29:44
  * @FilePath: /private-message-center/src/main.ts
  */
 import { NestFactory } from '@nestjs/core';
@@ -12,6 +12,6 @@ import { HttpExceptionFilter } from './shared/filters/http-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
-  await app.listen(3000);
+  await app.listen(2333);
 }
 bootstrap();
